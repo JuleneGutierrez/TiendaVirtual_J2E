8ab96f9b55d8b6f1dcf59ad14385d746ba6c47a9
daw212 <juleneguti2612@gmail.com>
2024-02-19 11:26:31 +0100

    Espero que funcione

--- src/java/conexion/Conexion.java
+++ src/java/conexion/Conexion.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
+ * No me mueras plisss
  * @author Julencia
  */
 //@WebServlet(name = "conexion", urlPatterns = {"/conexion"})
